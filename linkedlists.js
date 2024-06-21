4cbc8d8f8ac1f1ae137c568b23e92dae74583867
Fred <leferpz@gmail.com>
2024-06-21 18:19:59 -0400

    Added append and prepend methods

--- linkedlists.js
+++ linkedlists.js
@@ -1,3 +1,22 @@
-class Lists {
-    constructor()
+class LinkedList {
+
+    constructor(data, next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    append(value) {
+        new Node(value, null)
+    }
+
+    prepend(value) {
+        new Node(value, nextNode = LinkedList.next)
+    }
+}
+
+class Node {
+    constructor(value = null, nextNode = null) {
+        this.value = value;
+        this.nextNode = nextNode;
+    }
 }
